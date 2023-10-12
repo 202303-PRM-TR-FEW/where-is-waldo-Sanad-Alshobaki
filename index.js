e0b7e3ca68e8916280c5984dcea19c3d89dd4ba6
Sanad-Alshobaki <alshobakisanad@gmail.com>
2023-10-12 19:50:44 +0300

    solved

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
-
 const whereIsWaldo = (matrix) => {
-  
-}
+  for (let i = 0; i < matrix.length; i++) {
+    let item = matrix[0][0];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== item) {
+        console.log(`[${i + 1}][${j + 1}]`);
+      }
+    }
+  }
+};
+
+console.log("1");
+whereIsWaldo([
+  ["A", "A", "A"],
+  ["A", "A", "A"],
+  ["A", "B", "A"],
+]);
+
+console.log("3");
+whereIsWaldo([
+  ["c", "c", "c", "c"],
+  ["c", "c", "c", "d"],
+]);
+
+console.log("3");
+whereIsWaldo([
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["P", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+]);
